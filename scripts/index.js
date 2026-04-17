5239694b0579ec31bde05996cb9ab8660f894e89
federicoacker <acker.federico@gmail.com>
2026-04-17 14:33:55 +0200

    Iniziato a lavorare alla parte grafica di quando ci sono dei problemi, aggiunti skeleton loader di bulma come modo di gestire l'attesa dei dati

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,20 @@
 // @ts-check
 'use strict'
 
-const API_URL = "https://lanciweb.github.io/demo/api/pictures/";
+const API_URL = "https://lanciweb.github.iods/demo/api/pictures/";
 const polaroidWallEl = document.querySelector(".polaroid-wall")
-fetch(API_URL)
-    .then(response => response.json())
-    .then(json => generateList(json))
-    .catch(responseError => console.error(responseError.Error))
-    .finally(() => {console.log("Finished fetching, here's your data, or your error:")});
+
+document.addEventListener("DOMContentLoaded", fetchImages);
+
+
+function fetchImages() {
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(json => generateList(json))
+        .catch(responseError => alert(responseError))
+        .finally(() => { console.log("Finished fetching, here's your data, or your error:") });
+}
+
 
 
 /**
@@ -15,10 +22,10 @@ fetch(API_URL)
  */
 function createPolaroid(polaroidObject) {
     return `
-    <div class="column is-12-mobile is-6-tablet is-4-desktop is-2-ultrawide polaroid-wrapper">
+    <div class="column is-12-mobile is-6-tablet is-4-desktop is-2-ultrawide polaroid-wrapper ">
         <div class="polaroid d-flex flex-column" data-id="${polaroidObject.id}">
             <figure class="polaroid-image-wrapper">
-                <img src="${polaroidObject.url}" alt="${polaroidObject.title}" class="polaroid-image">
+                <img id="img-${polaroidObject.id}" src="${polaroidObject.url}" alt="${polaroidObject.title}" class="polaroid-image">
             </figure>
             <h2 class="sometype-mono">${polaroidObject.title}</h2>
             <p class="sometype-mono">${polaroidObject.date}</p>
@@ -30,11 +37,13 @@ function createPolaroid(polaroidObject) {
 /** 
  * @param {{ id: number, title: string, date: string, url: string }[]} jsonObject
 */
-function generateList(jsonObject){
-    const htmlStringArray = jsonObject.map(object => createPolaroid(object));
-    const htmlString = htmlStringArray.join("");
-    if(polaroidWallEl){
-        polaroidWallEl.innerHTML = htmlString;
-    }
-    
+function generateList(jsonObject) {
+    setTimeout(() => {
+        const htmlStringArray = jsonObject.map(object => createPolaroid(object));
+        const htmlString = htmlStringArray.join("");
+        if (polaroidWallEl) {
+            polaroidWallEl.innerHTML = htmlString;
+        }
+    }, 10 * 1000);
+
 }
