e7a7646680077d91e0f156d2753dd2e8dc6744ac
federicoacker <acker.federico@gmail.com>
2026-04-17 14:07:36 +0200

    creata funzione che crea la singola polaroid, semplcemente ricevendo un singolo object e restituendo la stringa html formattata come vogliamo per la polaroid. Creata funzione generateList, che riceve l'array di oggetti estratto dalla fetch, e ci usa sopra la map function per scorrere l'array e creare per ogni oggetto la string html, poi la mette in un array (per ora)

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,36 @@
+// @ts-check
+'use strict'
+
 const API_URL = "https://lanciweb.github.io/demo/api/pictures/";
 
 fetch(API_URL)
-.then(response => response.json())
-.then(json => console.log(json))
-.catch(responseError => console.error(response.Error))
-.finally(console.log("Finished fetching, here's your data, or your error:"));
+    .then(response => response.json())
+    .then(json => generateList(json))
+    .catch(responseError => console.error(responseError.Error))
+    .finally(() => {console.log("Finished fetching, here's your data, or your error:")});
+
+
+/**
+ * @param {{ id: number, title: string, date: string, url: string }} polaroidObject
+ */
+function createPolaroid(polaroidObject) {
+    return `
+    <div class="column is-12-mobile is-6-tablet is-4-desktop is-2-ultrawide polaroid-wrapper">
+        <div class="polaroid d-flex flex-column" data-id="${polaroidObject.id}">
+            <figure class="polaroid-image-wrapper">
+                <img src="${polaroidObject.url}" alt="${polaroidObject.title}" class="polaroid-image">
+            </figure>
+            <h3 class="sometype-mono">${polaroidObject.title}</h3>
+            <p class="sometype-mono">${polaroidObject.date}</p>
+        </div>
+    </div>
+    `;
+}
+
+/** 
+ * @param {{ id: number, title: string, date: string, url: string }[]} jsonObject
+*/
+function generateList(jsonObject){
+    const htmlStringArray = jsonObject.map(object => createPolaroid(object));
+    
+}
